d26285edabcf7f32176d9a801e308ff2addd2f54
Yevgeniy Sorokin <ewgenij87snwork@gmail.com>
2021-01-26 22:02:17 +0200

    complete view for all lists (genre_list.pug >> /catalog/genre/)

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -1,9 +1,17 @@
 var BookInstance = require('../models/bookinstance');
 
 // Display list of all BookInstances.
-exports.bookinstance_list = function(req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance list');
-};
+exports.bookinstance_list = function(req, res, next) {
+
+    BookInstance.find()
+      .populate('book')
+      .exec(function (err, list_bookinstances) {
+        if (err) { return next(err); }
+        // Successful, so render
+        res.render('bookinstance_list', { title: 'Book Instance List', bookinstance_list: list_bookinstances });
+      });
+  
+  };
 
 // Display detail page for a specific BookInstance.
 exports.bookinstance_detail = function(req, res) {
@@ -15,7 +23,7 @@ exports.bookinstance_create_get = function(req, res) {
     res.send('NOT IMPLEMENTED: BookInstance create GET');
 };
 
-// Handle BookInstance create on POST.
+// Handle BookInstance create on POST.s
 exports.bookinstance_create_post = function(req, res) {
     res.send('NOT IMPLEMENTED: BookInstance create POST');
 };

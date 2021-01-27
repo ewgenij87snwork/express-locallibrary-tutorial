37141b1564262abf24ef714462a5b8dbf0460b3a
Yevgeniy Sorokin <ewgenij87snwork@gmail.com>
2021-01-27 18:50:00 +0200

    FINISH: Express Tutorial Part 5: Displaying library data

--- models/user.js
+++ models/user.js
@@ -3,11 +3,11 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-    name: {type: String, required: true, minlength: 3, maxlength: 15},
-    role: {type: String, enum:['Library Worker', 'Director']}
+    name: { type: String, required: true, minlength: 3, maxlength: 15 },
+    role: { type: String, enum: ['Library Worker', 'Director'] }
 })
 
-UserSchema.virtual('position').get(function(){
+UserSchema.virtual('position').get(function () {
     return !role ? 'User' : role;
 })
 
--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('<h1>Users</h1>');
 });
-router.get('/cool', function(req, res, next) {
+router.get('/cool', function (req, res, next) {
   res.send('<h1>Cool</h1>');
 });
 module.exports = router;
